f41098529849ab20ed6d62ef5e762c942798cacc
Sophie Kaelin <48739273+sophieKaelin@users.noreply.github.com>
2024-12-05 21:35:07 +1100

    Finish Day 3 part 2

--- day03.py
+++ day03.py
@@ -17,3 +17,20 @@ for line in input:
             sum += int(nums[0]) * int(nums[1])
 
 print(sum)
+
+print("\n========== Part 2 ==========")
+
+sum = 0
+do = True
+for line in input:
+    for e in re.findall("mul\([0-9]+,[0-9]+\)|do\(\)|don\'t\(\)",line):
+        match e:
+            case "do()":
+                do = True
+            case "don't()":
+                do = False
+            case _:
+                nums = e[4:-1].split(",")
+                if do and (len(nums[0]) < 4 or len(nums[1]) < 4):
+                    sum += int(nums[0]) * int(nums[1])
+print(sum)
