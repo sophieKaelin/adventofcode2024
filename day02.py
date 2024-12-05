e426efbb465b89302511d054c45ee3fca4415588
Sophie Kaelin <48739273+sophieKaelin@users.noreply.github.com>
2024-12-05 20:57:46 +1100

    Day 2 part 2 (doesn't work)

--- day02.py
+++ day02.py
@@ -11,6 +11,9 @@ print("Input: " + str(input) + "\n")
 
 # ========== Common =========="
 
+def checkDiff(diff, isAsc):
+    return (diff < 0 and isAsc or diff > 0 and not isAsc) and abs(diff) in range(1,4)
+
 def isSafe(report):
     # Confirm the direction.
     isAsc = True
@@ -19,19 +22,46 @@ def isSafe(report):
     # Iterate through to find level diffs
     for i in range(1, len(report)):
         diff = report[i-1] - report[i]
-        if (diff < 0 and isAsc or diff > 0 and not isAsc) and abs(diff) in range(1,4):
+        if checkDiff(diff, isAsc):
             continue
         return i-1
         break
     return -1
 
 def isSafe2(report):
+    tolerated = False
+    # Confirm the direction.
+    isAsc = True
+    if report[0] > report[1]:
+        isAsc = False
+    # Iterate through to find level diffs
+    for i in range(1, len(report)):
+        diff = report[i-1] - report[i]
+        if checkDiff(diff, isAsc) or diff == 0:
+            continue
+        elif not tolerated and i < len(report)-1 and checkDiff(report[i-1] - report[i+1], isAsc):
+            tolerated = True
+            i+=1
+            continue
+        return i-1
+        break
+    return -1
 
 print("\n========== Part 1 ==========")
 
 safe = 0
 for report in input:
     if isSafe(report) == -1:
+        safe +=1
+
+print(safe)
+
+print("\n========== Part 2 ==========")
+
+safe = 0
+
+for report in input:
+    if isSafe2(report) == -1:
         safe +=1
 
 print(safe)
